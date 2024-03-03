3219b1f0dd02db00c43242854a954ab9d68345ca
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2024-03-03 17:35:24 +0400

    change

--- src/pages/home/header/Headline.js
+++ src/pages/home/header/Headline.js
@@ -3,23 +3,11 @@ import HeadlineOne from "./HeadlineOne";
 import HeadlineTwo from "./HeadlineTwo";
 
 function Headline() {
-  // const [cursorX, setCursorX] = useState();
-  // const [cursorY, setCursorY] = useState();
-
-  // window.addEventListener("mousemove", (e) => {
-  //   setCursorX(e.pageX);
-  //   setCursorY(e.pageY);
-  // });
-
   return (
     <article className="absolute bottom-0 h-full overflow-hidden w-full *:text-4xl md:*:text-9xl *:w-full *:my-5 *:font-normal">
       <div className="cursor-container h-full">
         <HeadlineOne />
         <HeadlineTwo />
-        {/* <div
-          className="cursor"
-          style={{ left: cursorX + "px", top: cursorY + "px" }}
-        ></div> */}
       </div>
     </article>
   );
